44c713c3cb290cc74886282d8fa293e3c00df39b
Strke12i <felipebagesteiro@gmail.com>
2021-09-04 17:50:01 -0300

    CRUD

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it "admin is valid" do
-    admin=Admin.new(email:"admin@admin.com",password:"123456")
+    admin=Admin.new(name:"Adm",email:"admin@admin.com",password:"123456")
 
     expect(admin.email).to eql "admin@adm.com"
 
